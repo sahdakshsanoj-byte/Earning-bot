0d6f87811a33a80429c0b4c4ad83f161ea4125dd
Daksh <sahdakshsanoj@gmail.com>
2026-04-20 08:11:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -60,7 +60,7 @@ const CONFIG = {
     SPONSORS: {
 
         slot1: {
-            active: true,
+            active: false,
             icon:   "💼",
             name:   "Sponsor Slot 1",
             desc:   "Contact admin to advertise here",
@@ -70,7 +70,7 @@ const CONFIG = {
         },
 
         slot2: {
-            active: true,
+            active: false,
             icon:   "📢",
             name:   "Sponsor Channel",
             desc:   "Join the channel & earn instantly",
@@ -80,7 +80,7 @@ const CONFIG = {
         },
 
         slot3: {
-            active: true,
+            active: false,
             icon:   "🌟",
             name:   "Partner Task",
             desc:   "Complete the task & claim to earn",
